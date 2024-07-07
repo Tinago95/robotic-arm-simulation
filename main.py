0d135bc36d5034f8c01cfa195a7bf2ec21360ba9
tinago95 <lt.ticharwa@gmail.com>
2024-07-08 00:28:56 +0800

    fix; added drawing object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Make sure to have the add-on "ZMQ remote API" running in
 # CoppeliaSim. Do not launch simulation, but run this script
-from simulate import actuate,stop_sim
+from simulate import actuate,stop_sim,sensing
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 
 print('Program started')
@@ -13,7 +13,10 @@ maxForce = 100
 # sim loop   
 try:
     while True :
+      sensing()
       actuate()
+    
+      
 except KeyboardInterrupt:
     stop_sim()
 
--- simulate.py
+++ simulate.py
@@ -2,30 +2,42 @@ import math
 import time
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 
-maxForce = 100
 
 client = RemoteAPIClient()
 sim = client.require('sim')
 sim.loadScene('/Users/lticharwa/Desktop/test-sim.ttt')
+endEffector = sim.getObject('/EndEffector')
+joint2 = sim.getObject('/Joint2')
+joint1 = sim.getObject('/Joint1')
 
+endEffectorTrace = sim.addDrawingObject(sim.drawing_linestrip, 5, 0, -1, 100000,[1,0,0])
 def moveToAngle(jointAngle, targetAngle):
         sim.setJointTargetPosition(jointAngle, targetAngle)
-        for _ in range(100):  # Adjust the range as needed
-            sim.step()
-            time.sleep(0.01)  # Adding a small delay to let the simulation process
+        sim.step()
+        time.sleep(0.01)  # Adding a small delay to let the simulation process
         
 def actuate ():
     # enable the stepping mode on the client:
     sim.setStepping(True)
     # start simulation
     sim.startSimulation()
-    
-    joint2 = sim.getObject('/Joint2')
-    joint1 = sim.getObject('/Joint1')
-    moveToAngle(joint2,math.pi/2)
     theta1 = sim.getJointPosition(joint1)
     theta2 = sim.getJointPosition(joint2)
     print('theta1:', theta1, 'theta2:', theta2)
+    t = sim.getSimulationTime() 
+    theta1 = math.pi*t/6
+    theta2 = math.pi*t/3
+    moveToAngle(joint2,theta2)
+    moveToAngle(joint1,theta1)
+    
+def sensing():    
+    effectorPos = sim.getObjectPosition(endEffector, sim.handle_world)
+    sim.addDrawingObjectItem(endEffectorTrace, effectorPos)
+    time.sleep(0.01)  # Adding a small delay to let the simulation process
+    
+    
+    
+   
     
 def stop_sim():
     sim.stopSimulation()
